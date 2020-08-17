75ea1194fac2236f752ce6d483d02829286e1678
utkarsh867 <utkarsh867@users.noreply.github.com>
2020-08-17 18:06:30 +0000

    deploy: 3f7e4d3db5420a977978a912d63336c67410cac9

---
+++ component---src-pages-aboutus-tsx-5fe4ea5f264d1bab5c6f.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{"6eH6":function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),r=n.n(a),u=n("Bl7J"),c=n("vrFN");t.default=function(){return r.a.createElement(u.a,null,r.a.createElement(c.a,{title:"About Us"}))}}}]);
+//# sourceMappingURL=component---src-pages-aboutus-tsx-5fe4ea5f264d1bab5c6f.js.map
